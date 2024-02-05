904a7ff269d7be9c1469cc969d6581b5eeaac6ba
jenniferYeeee <70613169+jenniferYeeee@users.noreply.github.com>
2024-02-04 19:52:54 -0500

    config github action

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://github.com/BlueJayRacing/baja_visualization/",
   plugins: [react()],
   resolve: {
     alias: {
